c218a4350666d2772124de04bafa2ac5e3e095c2
Kym Phillpotts <kphillpotts@gmail.com>
2015-02-03 23:28:02 +1100

    Project setup, main page and a navigation

---
+++ TrueOrFalse/TrueOrFalse/TrueOrFalse/Views/QuestionPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace TrueOrFalse.Views
+{
+    public partial class QuestionPage : ContentPage
+    {
+        public QuestionPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
