da700f6839455e91fecd12e38efbf9dc0391e42b
Kym Phillpotts <kphillpotts@gmail.com>
2015-07-06 21:28:38 +1000

    Add Question Model

---
+++ TrueOrFalse/TrueOrFalse/TrueOrFalse/Models/Question.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace TrueOrFalse
+{
+    public class Question
+    {
+        public enum Category
+        {
+            None = 0,
+            Nature = 1,
+            Sport = 2,
+            People = 3,
+            ManMade = 4
+        }
+
+        public Question (string text, bool answer, Category category)
+        {
+            QuestionText = text;
+            QuestionAnswer = answer;
+            QuestionCategory = category;
+        }
+
+        public string QuestionText {get; private set;}
+        public bool QuestionAnswer {get; private set;}
+        public Category QuestionCategory {get; private set;}
+
+    }
+}
+
